54cbee8deb71805fcfc2b5b10e345e20770c2fa3
parksoo0650 <blue4595@naver.com>
2017-01-23 08:52:00 +0900

    oop

--- src/hospital/index.java
+++ src/hospital/index.java
@@ -3,7 +3,7 @@ package hospital;
 public class index {
 
     public static void main(String[] args) {
-	Controller con = new Controller();
+	new Controller().hospital();
 	
     }
 }
--- src/test/index.java
+++ src/test/index.java
@@ -2,7 +2,6 @@ package test;
 
 public class index {
     public static void main(String[] args) { // local variables 반드시 initialize
-	Controller con = new Controller();
-	con.test();
+	new Controller().test();
     }
 }
